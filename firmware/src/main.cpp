bb6df9c28a04ed9da127d3b2e5281b30ec32980b
Pawel Tomaszewski <p.tomaszews2@partner.samsung.com>
2019-01-25 12:44:04 +0100

    bme280 initial setup

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,22 +1,25 @@
+#include <Wire.h>
 #include <Arduino.h>
+#include <Adafruit_BME280.h>
 
-
-int LED = 2;
-int counter = 0;
+Adafruit_BME280 bme;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(LED, OUTPUT);
-}
+  delay(1000);
 
-void loop() {
-  counter++;
-  String message = "Counter: " + String(counter) + "\n";
-  Serial.write(message.c_str());
+  Serial.println("HELLO THINGY v1.0");
 
-  digitalWrite(LED, HIGH);
-  delay(500);
-  digitalWrite(LED, LOW);
-  delay(500);
+  while(!bme.begin(0x76)) {
+    Serial.println("No BME280 sensor found.");
+    delay(1000);
+  }
   
+  Serial.println("Started.");
+}
+ 
+ 
+void loop() {
+  Serial.println("T: " + String(bme.readTemperature()) + ", P: " + String(bme.readPressure()) + ", H: " + String(bme.readHumidity()));
+  delay(1000);
 }
